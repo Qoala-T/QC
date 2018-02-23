af92558a929105e68bc5de88d132d59bfef16cbb
larawierenga <36070784+larawierenga@users.noreply.github.com>
2018-02-23 09:37:57 +0100

    Update Qoala_T_B_subset_based_github.R

--- Qoala_T_B_subset_based_github.R
+++ Qoala_T_B_subset_based_github.R
@@ -121,7 +121,7 @@ colnames(Qoala_T_predictions_subset_based) = c('VisitID','Scan_QoalaT', 'Recomme
   Qoala_T_predictions_subset_based$VisitID <- row.names(rf.probs)
   Qoala_T_predictions_subset_based$Scan_QoalaT <- rf.probs$Include*100 
   Qoala_T_predictions_subset_based$Recommendation <- rf.pred
-  Qoala_T_predictions_subset_based$manual_QC_adviced <- ifelse(Qoala_T_predictions_subset_based$Scan_QoalaT<60&Qoala_T_predictions_subset_based$Scan_QoalaT>40,1,0)
+  Qoala_T_predictions_subset_based$manual_QC_adviced <- ifelse(Qoala_T_predictions_subset_based$Scan_QoalaT<70&Qoala_T_predictions_subset_based$Scan_QoalaT>30,1,0)
   Qoala_T_predictions_subset_based <- Qoala_T_predictions_subset_based[order(Qoala_T_predictions_subset_based$Scan_QoalaT, Qoala_T_predictions_subset_based$VisitID),]
   
   
