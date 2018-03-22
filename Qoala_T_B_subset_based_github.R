61d3995de3ca3daacf2f5b493a83bc55ed2ab795
larawierenga <36070784+larawierenga@users.noreply.github.com>
2018-03-22 14:19:33 +0100

    Update Qoala_T_B_subset_based_github.R

--- Qoala_T_B_subset_based_github.R
+++ Qoala_T_B_subset_based_github.R
@@ -8,7 +8,8 @@
 # title: Qoala-T: A supervised-learning tool for quality control of automatic segmented MRI data
 #author:
 #  - name:   Klapwijk, E.T., van de Kamp, F., Meulen, M., Peters, S. and Wierenga, L.M.
-# 
+# http://doi.org/10.1101/278358
+#
 #  If you have any question or suggestion, dont hesitate to get in touch:
 #  l.m.wierenga@fsw.leidenuniv.nl
    
